a199b64911c5ff0e48766cac4f2fc481cac3f96e
developeritexpert3@gmail.com <developeritexpert3@gmail.com>
2025-08-14 17:14:37 +0530

    adding

--- app/components/WhySiggyComponents/WhyChooseSiggiy.tsx
+++ app/components/WhySiggyComponents/WhyChooseSiggiy.tsx
@@ -1,39 +1,41 @@
 import React from "react";
-import FeatureTextComponent from "../featuresComponents/FeatureTextComponent";
+import WhyTextComponent from "../WhySiggyComponents/WhyTextComponent";
 import Image from "next/image";
 const WhyChooseSiggiy = () => {
   return (
     <section className="px-[20px] py-[50px] md:py-[100px] md:px-[60px] text-white">
       <div className="container mx-auto">
         <div className="w-full flex flex-col md:flex-row-reverse md:justify-between items-center gap-[30px] md:gap-[79px] mt-[35px] md:mt-[112px]">
-          <div className="w-full md:w-1/2 max-w-[584px]">
-            <h2 className="w-full font-normal text-[24px]  md:text-[65px] md:leading-[75px] text-center max-w-[738px] mx-auto">
-              <span className="text-[#8ED3FA]"> Why </span>
+          <div className="w-full md:w-1/2 ">
+            <h2 className="w-full font-normal text-[24px]  md:text-[65px] md:leading-[75px] mb-[20px] mx-auto">
+               Why 
               Choose
               <span className="text-[#8ED3FA]"> Siggy?</span>
             </h2>
-            <p className="text-[14px] md:text-[18px] font-normal leading-[28px]">
+            <p className="text-[14px] md:text-[18px] font-normal leading-[28px] max-w-[561px] mb-[20px]">
               Because mobile connectivity shouldn’t be unreliable, slow, or
               unpredictable. Siggy is built to solve the real-world challenges
               that traditional networks can’t.
             </p>
-            <p className="text-[14px] md:text-[18px] font-normal leading-[28px]">
+            <p className="text-[14px] md:text-[18px] font-normal leading-[28px] max-w-[561px]">
               Our AI-powered platform ensures devices always connect to the
               strongest tower, delivering faster speeds, fewer drops, and a
               seamless experience for users, carriers, and enterprises alike.
             </p>
             <div className="w-full flex flex-col">
-              <FeatureTextComponent
-                imageParaText="Consumers: "
-                imageSpanText=" Fewer dropped calls, faster mobile data, stronger satisfaction."
+              <WhyTextComponent
+                imageParaText="Data-Driven Insights
+"
+               
               />
-              <FeatureTextComponent
-                imageParaText="Carriers:"
-                imageSpanText="Better network utilization, reduced churn, richer analytics."
+              <WhyTextComponent
+                imageParaText="Real-Time Optimization"
               />
-              <FeatureTextComponent
-                imageParaText="Enterprises:"
-                imageSpanText="Optimized field connectivity for mission-critical use cases"
+              <WhyTextComponent
+                imageParaText="Scalable & Future-Ready"
+              />
+        <WhyTextComponent
+                imageParaText="AI-Powered Tower Switching"
               />
             </div>
           </div>
---
+++ app/components/WhySiggyComponents/WhyTextComponent.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Image from "next/image"
+const WhyTextComponent = ({imageParaText}) => {
+  return (
+       <div className="flex flex-row gap-2  items-baseline">
+                      <Image src="/images/tick_features.png" width={14 } alt="tick-feature-img" height={13} className="w-[14px] h-[13px]"/>
+                      <p className="text-white text-[14px] md:text-[18px]  font-normal leading-[48px]">
+                        {imageParaText}
+                       </p>
+                     </div>
+              
+  )
+}
+
+export default WhyTextComponent
