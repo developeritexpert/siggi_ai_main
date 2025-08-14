55b9b0c7f3b14a40f282825c6bf83b9970f53696
developeritexpert3@gmail.com <developeritexpert3@gmail.com>
2025-08-14 13:12:18 +0530

    adding changes feature section

--- app/components/featuresComponents/FeatureHero.tsx
+++ app/components/featuresComponents/FeatureHero.tsx
@@ -5,7 +5,7 @@ const FeatureHero = () => {
       <section className="w-full pb-[149px] px-[20px] pt-[130px] md:pt-[226px] md:pb-[178px] 2xl:pb-[201px] 2xl:bg-bottom  md:px-[60px]
              bg-[url('/assests/HeroSection.svg')]  bg-no-repeat bg-cover ">
              <div className="container mx-auto">
-             <h1 className="text-[36px] leading-[40px]   max-w-[295px] md:text-[90px] md:leading-[89px] max-w-[665px] font-normal text-white">
+             <h1 className="text-[36px] leading-[40px]   max-w-[295px] md:text-[90px] md:leading-[89px] md:max-w-[665px] font-normal text-white">
                 Packed with Power. Built for Everyone
              </h1>
              <p className="font-normal leading-[24px] mt:[16px] text-[16px] md:text-[18px] md:leading-[28px] max-w-[524px] md:mt-[18px]">
